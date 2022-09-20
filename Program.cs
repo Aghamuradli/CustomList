2b89f7d7db5e799c49513df941fc8d986dc97dfa
Babek Aghamuradov <babek.aghamuradli@gmail.com>
2022-09-20 23:06:40 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Babek");
+            Console.WriteLine("----------myList.Length----------");
+
+            Console.WriteLine(myList.Length);
+
+            //Elave olunan elementin deqiqliyinin yoxlanlasi
+
+            myList.Add("Kenan");
+            Console.WriteLine(myList.Length);
+            Console.WriteLine("----------myList.Items----------");
+
+            //Elave olunan adlarin siyahsi
+
+            foreach (var names in myList.Items)
+            {
+                Console.WriteLine(names);
+            }
+
+        }
+    }
+}
